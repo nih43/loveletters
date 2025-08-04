3db5e9baf19db007faa8745c8bf949840d846528
Niklas Hollinderbäumer <niklas.hollinderbaeumer@stud.hn.de>
2025-08-04 22:57:06 +0200

    wip

--- app.py
+++ app.py
@@ -8,13 +8,13 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     today = date.today().isoformat()
-    letter_path = os.path.join("letters", f"{today}.txt")
-    if os.path.exists(letter_path):
-        with open(letter_path, "r", encoding="utf-8") as f:
+    question_path = os.path.join("questions", f"{today}.txt")
+    if os.path.exists(question_path):
+        with open(question_path, "r", encoding="utf-8") as f:
             content = f.read()
     else:
-        content = "Heute ist kein Brief da – vielleicht morgen 😊"
-    return render_template("index.html", letter=content)
+        content = "Heute ist keine Frage da – vielleicht morgen 😊"
+    return render_template("index.html", question=content)
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=3005)
+    app.run(host="0.0.0.0", port=3005, debug=True)
